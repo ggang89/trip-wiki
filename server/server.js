53c75ab19021918eacb500240992e7d8da8c122b
ggang89 <ggang89@gmail.com>
2024-11-11 15:30:34 +0900

    express 설치, 코드 작성

---
+++ server/server.js
@@ -0,0 +1,23 @@
+//express 모듈 가져오기
+const express = require("express");
+
+//경로 쉽게 설정해주는 path 모듈 가져오기
+const path = require("path");
+
+//express 모듈 실행해서 app에 할당
+const app = express();
+
+//이전에 3000번 서버쓴 기록이 있어서 오류뜸=>4000으로 임으로 바꿔줌
+const PORT = 4000;
+
+//서버가 접근할 수 있도록 경로 지정
+app.use(express.static(path.join(__dirname, "..")));
+
+//모든 요청에 동일한 index.html객체 반환
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("start server");
+});
--- src/components/RegionList.js
+++ src/components/RegionList.js
@@ -1,4 +1,16 @@
 export default function RegionList() {
   
-  
+   this.$target = document.createElement("div");
+   this.$target.className = "region-list";
+
+   this.template = () => {};
+
+   this.render = () => {};
+
+   this.setState = (newState) => {
+     this.state = newState;
+     this.render();
+   };
+
+   this.render();
 }
