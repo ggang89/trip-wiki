baea3ead51760a7c3da7e0836f5e4b8abcae6cc1
ggang89 <ggang89@gmail.com>
2024-11-11 16:27:03 +0900

    App 컴포넌트 작성 => 필요한 컴포넌트 불러와서 변수에 넣고, 초기 state값 지정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
-export default function App() {
-  
+import CityDetail from "./components/CityDetail.js";
+import CityList from "./components/CityList.js";
+import Header from "./components/Header.js"
+import RegionList from "./components/RegionList.js";
+
+export default function App($app) {
+  this.state = {
+    startIdx: 0,
+    sortBy: '',
+    searchWord: '',
+    region: '',
+    cities:'',
+  };
+
+  const header = new Header();
+  const regionList = new RegionList();
+  const cityList = new CityList({$app,initialState:this.state.cities});
+  const cityDetail = new CityDetail();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    cityList.setState(this.state.cities);
+  }
+
+  const init = () => { 
+
+  };
+
+  init();
 }
