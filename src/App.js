d30403906518b324fd3c4c0a16c41f217f3d33d4
ggang89 <ggang89@gmail.com>
2024-11-13 17:31:56 +0900

    regionList 페이지 구성 => 지역선택해도 안 바뀜(오류)

--- src/App.js
+++ src/App.js
@@ -90,7 +90,24 @@ export default function App($app) {
     },
   });
 
-  const regionList = new RegionList();
+  const regionList = new RegionList({
+    $app,
+    initialState: this.state.region,
+    handleRegion: async (region) => {
+      history.pushState(null, null, `/${region}?sort=total`);
+      const cities = await request(0, region, "total");
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: "total",
+        region: region,
+        cities: cities,
+        searchWord: "",
+      });
+    },
+  });
+
+
   const cityList = new CityList({
     $app,
     initialState: this.state.cities,
@@ -124,7 +141,32 @@ export default function App($app) {
       sortBy: this.state.sortBy,
       searchWord: this.state.searchWord,
     });
+    regionList.setState(this.state.region);
   };
+
+  window.addEventListener('popstate', async () => {
+    const urlPath = window.location.pathname;
+
+    const prevRegion = urlPath.replace('/', '');
+    const prevSortBy = getSortBy();
+    const prevSearchWord = getSearchWord();
+    const prevStartIdx = 0;
+    const prevCities = await request(
+      prevStartIdx,
+      prevRegion,
+      prevSortBy,
+      prevSearchWord
+    );
+  
+    this.setState({
+      ...this.state,
+      startIdx: prevStartIdx,
+      sortBy: prevSortBy,
+      region: prevRegion,
+      searchWord: prevSearchWord,
+      cities: prevCities,
+    });
+  });
 
   const init = async () => {
     const cities = await request(
--- src/components/RegionList.js
+++ src/components/RegionList.js
@@ -1,16 +1,54 @@
-export default function RegionList() {
-  
+export default function RegionList({ $app, initialState, handleRegion }) {
+   this.state = initialState;
    this.$target = document.createElement("div");
    this.$target.className = "region-list";
 
-   this.template = () => {};
+   this.handleRegion = handleRegion;
+   $app.appendChild(this.$target);
 
-   this.render = () => {};
+  this.template = () => {
+    const regionList = [
+      "🚀 All",
+      "🌏 Asia",
+      "🕌 Middle-East",
+      "🏛 Europe",
+      "💃 Latin-America",
+      "🐘 Africa",
+      "🏈 North-America",
+      "🏄 Oceania",
+    ];
+    let temp = ``;
+    regionList.forEach((elm) => {
+      let regionId = elm.split(" ")[1]; //이모지 제외한 지역명 추출
+      temp += `<div id=${regionId}>${elm}</div>`;
+    });
+    return temp;
+  };
 
-   this.setState = (newState) => {
-     this.state = newState;
-     this.render();
-   };
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+    let $currentRegion;
+    if (this.state) {
+      $currentRegion = document.getElementById(this.state);
 
-   this.render();
+      //currentRegion값이 존재하면 그 값에 clicked 추가
+      $currentRegion && ($currentRegion.className = "clicked");
+    } else {
+     document.getElementById("All").className = "clicked";
+    }
+
+    const $regionList = this.$target.querySelectorAll("div");
+    $regionList.forEach((elm) => {
+      elm.addEventListener("click", () => {
+        this.handleRegion(elm.id);
+      });
+    });
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
 }
